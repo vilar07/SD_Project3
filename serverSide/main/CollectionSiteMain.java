60406352af4f9755daabca034d8b3b90a244b65c
Diogo Santos <diogoejsantos@ua.pt>
2023-05-19 20:04:45 +0100

    CollectionSiteMain

--- serverSide/main/CollectionSiteMain.java
+++ serverSide/main/CollectionSiteMain.java
@@ -1,165 +1,244 @@
 package serverSide.main;
 
-import serverSide.entities.*;
-import serverSide.interfaces.CollectionSiteInterface;
+import interfaces.*;
 import serverSide.objects.*;
-import commInfra.*;
-import java.net.*;
+import utils.Constants;
 
-import clientSide.stubs.AssaultPartyStub;
-import clientSide.stubs.GeneralRepositoryStub;
-import clientSide.stubs.MuseumStub;
+import java.rmi.AlreadyBoundException;
+import java.rmi.NoSuchObjectException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
 
 /**
- *    Collection Site server of the Heist To The Museum.
+ *    General Repository server of the Heist To The Museum.
  * <p>
  *    Implementation of a client-server model of type 2 (server replication).
  *    Communication is based on a communication channel under the TCP protocol.
  */
-public class CollectionSiteMain
-{
-  /**
-   *  Flag signaling the service is active.
-   */
-   public static boolean waitConnection;
+public class CollectionSiteMain {
+    /**
+     *  Flag signaling the end of operations.
+     */
+    private static boolean end = false;
 
-  /**
-   *  Main method.
-   *
-   *    @param args runtime arguments
-   *        args[0] - port number for listening to service requests
-   *        args[1] - name of the platform where is located the server for
-   *             the general repository
-   *        args[2] - port number where the server for the general repository
-   *             is listening to service requests
-   *        args[3] - name of the platform where is located the server for
-   *             the assault party 0
-   *        args[4] - port number where the server for the assault party 0
-   *             is listening to service requests
-   *        args[5] - name of the platform where is located the server for
-   *             the assault party 1
-   *        args[6] - port number where the server for the assault party 1
-   *             is listening to service requests
-   *        args[7] - name of the platform where is located the server for
-   *             the museum
-   *        args[8] - port number where the server for the museum
-   *             is listening to service requests
-   */
-   public static void main (String [] args)
-   {
-      CollectionSite collectionSite;
-      ServerCom serverCom, serverComi;                                         // communication channels
-      int portNumber = -1;                                             // port number for listening to service requests
-      String generalRepositoryHostName = null;
-      int generalRepositoryPortNumber = -1;
-      String assaultParty0HostName = null;
-      int assaultParty0PortNumber = -1;
-      String assaultParty1HostName = null;
-      int assaultParty1PortNumber = -1;
-      String museumHostName = null;
-      int museumPortNumber = -1;
+    /**
+     *  Main method.
+     * <p>
+     *        args[0] - port number for listening to service requests
+     *        args[1] - name of the platform where is located the RMI registering service
+     *        args[2] - port number where the registering service is listening to service requests
+     */
+    public static void main (String [] args) {
+        int portNumb = -1;                                             // port number for listening to service requests
+        String rmiRegHostName;                                         // name of the platform where is located the RMI registering service
+        int rmiRegPortNumb = -1;                                       // port number where the registering service is listening to service requests
 
-      if (args.length != 9)
-         {  System.out.println("Wrong number of parameters!");
-           System.exit(1);
-         }
-      try
-      { portNumber = Integer.parseInt (args[0]);
-      }
-      catch (NumberFormatException e)
-      { System.out.println(args[0] + " is not a number!");
-        System.exit(1);
-      }
-      if ((portNumber < 4000) || (portNumber >= 65536))
-         { System.out.println(args[0] + " is not a valid port number!");
-           System.exit(1);
-         }
-
-      generalRepositoryHostName = args[1];
-      try
-      { 
-        generalRepositoryPortNumber = Integer.parseInt (args[2]);
-      }
-      catch (NumberFormatException e)
-      { System.out.println(args[2] + " is not a number!");
-        System.exit(1);
-      }
-      if ((generalRepositoryPortNumber < 4000) || (generalRepositoryPortNumber >= 65536))
-        { System.out.println(args[2] + " is not a valid port number!");
-          System.exit(1);
-        }
-
-      assaultParty0HostName = args[3];
-      try
-      { 
-        assaultParty0PortNumber = Integer.parseInt (args[4]);
-      }
-      catch (NumberFormatException e)
-      { System.out.println(args[4] + " is not a number!");
-        System.exit(1);
-      }
-      if ((assaultParty0PortNumber < 4000) || (assaultParty0PortNumber >= 65536))
-        { System.out.println(args[4] + " is not a valid port number!");
-          System.exit(1);
-        }
-
-      assaultParty1HostName = args[5];
-      try
-      { 
-        assaultParty1PortNumber = Integer.parseInt (args[6]);
-      }
-      catch (NumberFormatException e)
-      { System.out.println(args[6] + " is not a number!");
-        System.exit(1);
-      }
-      if ((assaultParty1PortNumber < 4000) || (assaultParty1PortNumber >= 65536))
-          { System.out.println(args[6] + " is not a valid port number!");
+        if (args.length != 3) {
+            System.out.println("Wrong number of parameters!");
             System.exit(1);
-          }
-
-      museumHostName = args[7];
-      try
-      { 
-        museumPortNumber = Integer.parseInt (args[8]);
-      }
-      catch (NumberFormatException e)
-      { System.out.println(args[8] + " is not a number!");
-        System.exit(1);
-      }
-      if ((museumPortNumber < 4000) || (museumPortNumber >= 65536))
-          { System.out.println(args[8] + " is not a valid port number!");
-            System.exit(1);
-          }
-
-      /* service is established */
-
-      // Shared regions initialization
-      GeneralRepositoryStub generalRepository = new GeneralRepositoryStub(generalRepositoryHostName, generalRepositoryPortNumber);
-      AssaultPartyStub[] assaultParties = {new AssaultPartyStub(0, assaultParty0HostName, assaultParty0PortNumber),
-                                           new AssaultPartyStub(1, assaultParty1HostName, assaultParty1PortNumber)};
-      MuseumStub museum = new MuseumStub(museumHostName, museumPortNumber);
-      collectionSite = new CollectionSite(generalRepository, assaultParties, museum);
-      CollectionSiteInterface collectionSiteInterface = new CollectionSiteInterface(collectionSite);
-
-      serverCom = new ServerCom(portNumber);                                         // listening channel at the public port is established
-      serverCom.start();
-      System.out.println("Service is established!");
-      System.out.println("Server is listening for service requests.");
-
-     /* service request processing */
-
-      CollectionSiteProxyAgent proxy;                                // service provider agent
-
-      waitConnection = true;
-      while (waitConnection)
-      { try
-        { serverComi = serverCom.accept();                                    // enter listening procedure
-          proxy = new CollectionSiteProxyAgent(serverComi, collectionSiteInterface);    // start a service provider agent to address
-          proxy.start();                                                      //   the request of service
         }
-        catch (SocketTimeoutException ignored) {}
-      }
-      serverCom.end();                                                   // operations termination
-      System.out.println("Server was shutdown.");
-   }
+        try {
+            portNumb = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("args[0] is not a number!");
+            System.exit(1);
+        }
+        if ((portNumb < 4000) || (portNumb >= 65536)) {
+            System.out.println("args[0] is not a valid port number!");
+            System.exit(1);
+        }
+        rmiRegHostName = args[1];
+        try {
+            rmiRegPortNumb = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("args[2] is not a number!");
+            System.exit(1);
+        }
+        if ((rmiRegPortNumb < 4000) || (rmiRegPortNumb >= 65536)) {
+            System.out.println("args[2] is not a valid port number!");
+            System.exit(1);
+        }
+
+        /* create and install the security manager */
+
+        if (System.getSecurityManager() == null)
+            System.setSecurityManager(new SecurityManager());
+        System.out.println("Security manager was installed!");
+
+        /* get a remote reference to the general repository, assault party and museum objects */
+
+        String nameEntryGeneralRepository = "GeneralRepository";
+        GeneralRepositoryInterface generalRepositoryStub = null;
+        String nameEntryAssaultParty0 = "AssaultParty0";
+        String nameEntryAssaultParty1 = "AssaultParty1";
+        AssaultPartyInterface[] assaultPartyStubs = {null, null};
+        String nameEntryMuseum = "Museum";
+        MuseumInterface museumStub = null;
+
+        Registry registry = null;
+        try {
+            registry = LocateRegistry.getRegistry(rmiRegHostName, rmiRegPortNumb);
+        } catch (RemoteException e) {
+            System.out.println("RMI registry creation exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("RMI registry was created!");
+
+        try {
+            generalRepositoryStub = (GeneralRepositoryInterface) registry.lookup(nameEntryGeneralRepository);
+        } catch (RemoteException e) {
+            System.out.println("General Repository lookup exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NotBoundException e) {
+            System.out.println("General Repository not bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            assaultPartyStubs[0] = (AssaultPartyInterface) registry.lookup(nameEntryAssaultParty0);
+        } catch (RemoteException e) {
+            System.out.println("Assault Party 0 lookup exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NotBoundException e) {
+            System.out.println("Assault Party 0 not bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            assaultPartyStubs[1] = (AssaultPartyInterface) registry.lookup(nameEntryAssaultParty1);
+        } catch (RemoteException e) {
+            System.out.println("Assault Party 1 lookup exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NotBoundException e) {
+            System.out.println("Assault Party 1 not bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            museumStub = (MuseumInterface) registry.lookup(nameEntryMuseum);
+        } catch (RemoteException e) {
+            System.out.println("Museum lookup exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NotBoundException e) {
+            System.out.println("Museum not bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        /* instantiate a collection site object */
+
+        CollectionSite collectionSite = new CollectionSite(generalRepositoryStub, assaultPartyStubs, museumStub);
+        CollectionSiteInterface collectionSiteStub = null;
+        try {
+            collectionSiteStub = (CollectionSiteInterface) UnicastRemoteObject.exportObject(collectionSite, portNumb);
+        } catch (RemoteException e) {
+            System.out.println("Collection Site stub generation exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Stub was generated!");
+
+        /* register it with the general registry service */
+
+        String nameEntryBase = "RegisterHandler";                  // public name of the object that enables the registration
+        // of other remote objects
+        String nameEntryObject = "CollectionSite";                 // public name of the collection site object
+        Register reg = null;                                       // remote reference to the object that enables the registration
+        // of other remote objects
+        try {
+            reg = (Register) registry.lookup(nameEntryBase);
+        } catch (RemoteException e) {
+            System.out.println("RegisterRemoteObject lookup exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NotBoundException e) {
+            System.out.println("RegisterRemoteObject not bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            reg.bind(nameEntryObject, collectionSiteStub);
+        } catch (RemoteException e) {
+            System.out.println("Collection Site registration exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (AlreadyBoundException e) {
+            System.out.println("Collection Site already bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Collection Site object was registered!");
+
+        /* wait for the end of operations */
+
+        System.out.println("Collection Site is in operation!");
+        try {
+            while (!end)
+                synchronized (Class.forName("serverSide.main.CollectionSiteMain")) {
+                    try {
+                        (Class.forName("serverSide.main.CollectionSiteMain")).wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("Collection Site main thread was interrupted!");
+                    }
+                }
+        } catch (ClassNotFoundException e) {
+            System.out.println("The data type CollectionSiteMain was not found (blocking)!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        /* server shutdown */
+
+        boolean shutdownDone = false; // flag signalling the shutdown of the assault party 0 service
+
+        try {
+            reg.unbind(nameEntryObject);
+        } catch (RemoteException e) {
+            System.out.println("Collection Site deregistration exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NotBoundException e) {
+            System.out.println("Collection Site not bound exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Collection Site was deregistered!");
+
+        try {
+            shutdownDone = UnicastRemoteObject.unexportObject(collectionSite, true);
+        } catch (NoSuchObjectException e) {
+            System.out.println("Collection Site unexport exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        if (shutdownDone)
+            System.out.println("Collection Site was shutdown!");
+    }
+
+    public static void shutdown() {
+        end = true;
+        try {
+            synchronized (Class.forName("serverSide.main.CollectionSiteMain")) {
+                (Class.forName("serverSide.main.CollectionSiteMain")).notify();
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("The data type CollectionSite was not found (waking up)!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 }
