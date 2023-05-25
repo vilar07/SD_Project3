d44a30633f752526df03573732f89186962575a6
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 09:51:37 +0100

    GenReposDeployAndRun.sh

--- GenReposDeployAndRun.sh
+++ GenReposDeployAndRun.sh
@@ -4,7 +4,7 @@ sshpass -f password ssh sd107@l040101-ws02.ua.pt 'rm -rf test/HeistToTheMuseum/*
 sshpass -f password scp dirGeneralRepos.zip sd107@l040101-ws02.ua.pt:test/HeistToTheMuseum
 echo "Decompressing data sent to the general repository node."
 sshpass -f password ssh sd107@l040101-ws02.ua.pt 'cd test/HeistToTheMuseum ; unzip -uq dirGeneralRepos.zip'
-echo "Executing program at the server general repository."
-sshpass -f password ssh sd107@l040101-ws02.ua.pt 'cd test/HeistToTheMuseum/dirGeneralRepos ; java serverSide.main.GeneralRepositoryMain 22162'
+echo "Executing program at the general repository node."
+sshpass -f password ssh sd107@l040101-ws02.ua.pt 'cd test/HeistToTheMuseum/dirGeneralRepos ; ./repos_com_d.sh sd107'
 echo "Server shutdown."
 sshpass -f password ssh sd107@l040101-ws02.ua.pt 'cd test/HeistToTheMuseum/dirGeneralRepos ; less heist.log'
