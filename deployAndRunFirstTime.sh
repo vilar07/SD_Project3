ca6ac24f1bf0ee33a7aab9dfe6b1ae2f0ccedf29
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 12:41:13 +0100

    increased deploy times

--- deployAndRunFirstTime.sh
+++ deployAndRunFirstTime.sh
@@ -3,16 +3,16 @@ sleep 8
 xterm  -T "Registry" -hold -e "./RegistryDeployAndRun.sh" &
 sleep 8
 xterm  -T "General Repository" -hold -e "./GenReposDeployAndRun.sh" &
-sleep 2
+sleep 4
 xterm  -T "Assault Party 0" -hold -e "./AP0DeployAndRun.sh" &
-sleep 2
+sleep 4
 xterm  -T "Assault Party 1" -hold -e "./AP1DeployAndRun.sh" &
-sleep 2
+sleep 4
 xterm  -T "Museum" -hold -e "./MuseumDeployAndRun.sh" &
-sleep 2
+sleep 4
 xterm  -T "Collection Site" -hold -e "./CollSiteDeployAndRun.sh" &
-sleep 2
+sleep 4
 xterm  -T "Concentration Site" -hold -e "./ConcSiteDeployAndRun.sh" &
-sleep 1
+sleep 4
 xterm  -T "Clients" -hold -e "./ClientDeployAndRun.sh" &
 # increase sleep times if needed
