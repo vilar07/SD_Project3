3b59aa955294935379dde88cf27146cba45a19a3
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 09:52:39 +0100

    ClientDeployAndRun.sh

--- ClientDeployAndRun.sh
+++ ClientDeployAndRun.sh
@@ -1,8 +1,8 @@
-echo "Transfering data to the Client node."
+echo "Transfering data to the client node."
 sshpass -f password ssh sd107@l040101-ws09.ua.pt 'mkdir -p test/HeistToTheMuseum'
 sshpass -f password ssh sd107@l040101-ws09.ua.pt 'rm -rf test/HeistToTheMuseum/*'
 sshpass -f password scp dirClient.zip sd107@l040101-ws09.ua.pt:test/HeistToTheMuseum
-echo "Decompressing data sent to the Client Site node."
+echo "Decompressing data sent to the client node."
 sshpass -f password ssh sd107@l040101-ws09.ua.pt 'cd test/HeistToTheMuseum ; unzip -uq dirClient.zip'
-echo "Executing program at the Client Site node."
-sshpass -f password ssh sd107@l040101-ws09.ua.pt 'cd test/HeistToTheMuseum/dirClient ; java clientSide.main.HeistToTheMuseum 22168 l040101-ws02.ua.pt 22162 l040101-ws03.ua.pt 22163 l040101-ws05.ua.pt 22164 l040101-ws06.ua.pt 22165 l040101-ws07.ua.pt 22166 l040101-ws08.ua.pt 22167'
+echo "Executing program at the client node."
+sshpass -f password ssh sd107@l040101-ws09.ua.pt 'cd test/HeistToTheMuseum/dirClient ; ./client_com_d.sh sd107'
