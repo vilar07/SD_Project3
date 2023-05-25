828f9711c99b3b84e4f53c47032d377c59ec6d3f
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 20:07:31 +0100

    GeneralRepositoryInterface javadoc

--- interfaces/GeneralRepositoryInterface.java
+++ interfaces/GeneralRepositoryInterface.java
@@ -9,91 +9,110 @@ import java.rmi.RemoteException;
 public interface GeneralRepositoryInterface extends Remote {
     /**
      * Prints the state of the simulation to the logging file.
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void printState() throws RemoteException;
+    void printState() throws RemoteException;
 
     /**
      * Prints the tail of the logging file.
-     * @param total number of paintings acquired.
+     * @param total number of paintings acquired
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void printTail(int total) throws RemoteException;
+    void printTail(int total) throws RemoteException;
 
     /**
      * Sets the Master Thief state.
-     * @param state the state code to change to.
+     * @param state the state code to change to
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setMasterThiefState(int state) throws RemoteException;
+    void setMasterThiefState(int state) throws RemoteException;
 
     /**
      * Sets the Ordinary Thief state.
-     * @param id the identification of the thief.
-     * @param state the state code to change to.
-     * @param maxDisplacement the maximum displacement of the thief.
+     * @param id the identification of the thief
+     * @param state the state code to change to
+     * @param maxDisplacement the maximum displacement of the thief
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setOrdinaryThiefState(int id, int state, int maxDisplacement) throws RemoteException;
+    void setOrdinaryThiefState(int id, int state, int maxDisplacement) throws RemoteException;
 
     /**
      * Sets the Ordinary Thief state (version 2).
-     * @param id the identification of the thief.
-     * @param state the state code to change to.
+     * @param id the identification of the thief
+     * @param state the state code to change to
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setOrdinaryThiefState(int id, int state) throws RemoteException;
+    void setOrdinaryThiefState(int id, int state) throws RemoteException;
 
     /**
      * Sets the Assault Party room target.
-     * @param party the party number.
-     * @param room the room identification.
+     * @param party the party number
+     * @param room the room identification
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setAssaultPartyRoom(int party, int room) throws RemoteException;
+    void setAssaultPartyRoom(int party, int room) throws RemoteException;
 
     /**
      * Sets an Assault Party member.
-     * @param party the party number.
-     * @param thief the identification of the thief.
-     * @param pos the present position of the thief.
-     * @param cv 1 if the thief is carrying a canvas, 0 otherwise.
+     * @param party the party number
+     * @param thief the identification of the thief
+     * @param pos the present position of the thief
+     * @param cv 1 if the thief is carrying a canvas, 0 otherwise
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setAssaultPartyMember(int party, int thief, int pos, int cv) throws RemoteException;
+    void setAssaultPartyMember(int party, int thief, int pos, int cv) throws RemoteException;
 
     /**
      * Removes an Assault Party member.
-     * @param party the identification of the Assault Party.
-     * @param thief the identification of the Ordinary Thief.
+     * @param party the identification of the Assault Party
+     * @param thief the identification of the Ordinary Thief
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void removeAssaultPartyMember(int party, int thief) throws RemoteException;
+    void removeAssaultPartyMember(int party, int thief) throws RemoteException;
 
     /**
      * Resets the Assault Party logging details.
-     * @param party the party number.
+     * @param party the party number
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void disbandAssaultParty(int party) throws RemoteException;
+    void disbandAssaultParty(int party) throws RemoteException;
 
     /**
      * Sets the room state.
-     * @param id the room identification.
-     * @param paintings the number of paintings.
-     * @param distance the distance to the outside gathering site.
+     * @param id the room identification
+     * @param paintings the number of paintings
+     * @param distance the distance to the outside gathering site
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setRoomState(int id, int paintings, int distance) throws RemoteException;
+    void setRoomState(int id, int paintings, int distance) throws RemoteException;
 
     /**
      * Sets the room state.
-     * @param id the room identification.
-     * @param paintings the number of paintings.
+     * @param id the room identification
+     * @param paintings the number of paintings
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setRoomState(int id, int paintings) throws RemoteException;
+    void setRoomState(int id, int paintings) throws RemoteException;
 
     /**
      * Sets the initial room states.
-     * @param paintings an array with the number of paintings of each room.
-     * @param distances an array with the distance to each room.
+     * @param paintings an array with the number of paintings of each room
+     * @param distances an array with the distance to each room
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setInitialRoomStates(int[] paintings, int[] distances) throws RemoteException;
+    void setInitialRoomStates(int[] paintings, int[] distances) throws RemoteException;
 
-    public void setAttributesOfOrdinaryThieves(int[] maxDisplacements) throws RemoteException;
+    /**
+     * Sets the initial attributes of the thieves.
+     * @param maxDisplacements an array with the maximum displacements of the Ordinary Thieves, where the index of each
+     *                         value is the identification of the thief
+     * @throws RemoteException if the execution of the remote code failed
+     */
+    void setAttributesOfOrdinaryThieves(int[] maxDisplacements) throws RemoteException;
 
     /**
      * Sends the shutdown signal to the General Repository.
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void shutdown() throws RemoteException;
+    void shutdown() throws RemoteException;
 }
