c42d5a2c0994e692813f5c5222dadcc39395004d
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 19:55:08 +0100

    javador AssaultPartyInterface

--- interfaces/AssaultPartyInterface.java
+++ interfaces/AssaultPartyInterface.java
@@ -13,102 +13,125 @@ import java.rmi.RemoteException;
 public interface AssaultPartyInterface extends Remote {
     /**
      * Getter for the identification of the Assault Party.
-     * @return the identification of the Assault Party.
+     * @return the identification of the Assault Party
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public int getID() throws RemoteException;
+    int getID() throws RemoteException;
 
     /**
      * Called by the Master Thief to send the Assault Party to the museum.
      * After that call, Assault Party can start crawling.
+     * @return the updated state of the Master Thief
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public int sendAssaultParty() throws RemoteException;
+    int sendAssaultParty() throws RemoteException;
 
     /**
      * Called by the Ordinary Thief to crawl in.
-     * @return false if they have finished the crawling.
+     * @param ordinaryThief the identification of the Ordinary Thief
+     * @param ordinaryThiefMaxDisplacement the maximum displacement of the Ordinary Thief
+     * @return a ReturnBoolean reference data type with the value false if they have finished the crawling and the
+     * updated state of the Ordinary Thief
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public ReturnBoolean crawlIn(int ordinaryThief, int ordinaryThiefMaxDisplacement) throws RemoteException;
+    ReturnBoolean crawlIn(int ordinaryThief, int ordinaryThiefMaxDisplacement) throws RemoteException;
 
     /**
      * Called to awake the first member in the line of Assault Party, by the last party member that rolled a canvas,
      * so that the Assault Party can crawl out.
      * - Synchronization Point between members of the Assault Party.
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void reverseDirection() throws RemoteException;
+    void reverseDirection() throws RemoteException;
 
     /**
      * Called by the Ordinary Thief to crawl out.
-     * @return false if they have finished the crawling.
+     * @param ordinaryThief the identification of the Ordinary Thief
+     * @param ordinaryThiefMaxDisplacement the maximum displacement of the Ordinary Thief
+     * @return a ReturnBoolean reference data type with the value false if they have finished the crawling and the
+     * updated state of the Ordinary Thief
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public ReturnBoolean crawlOut(int ordinaryThief, int ordinaryThiefMaxDisplacement) throws RemoteException;
+    ReturnBoolean crawlOut(int ordinaryThief, int ordinaryThiefMaxDisplacement) throws RemoteException;
 
     /**
      * Sends the shutdown signal to the Assault Party.
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void shutdown() throws RemoteException;
+    void shutdown() throws RemoteException;
  
     /**
      * Getter for the room destination.
-     * @return the room identification.
+     * @return the identification of the room
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public int getRoom() throws RemoteException;
+    int getRoom() throws RemoteException;
 
     /**
      * Setter for the busy hands attribute of a thief.
-     * @param ordinaryThief the identification of the Ordinary Thief.
-     * @param res true if they have a canvas, false otherwise.
+     * @param ordinaryThief the identification of the Ordinary Thief
+     * @param res true if they have a canvas, false otherwise
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setBusyHands(int ordinaryThief, boolean res) throws RemoteException;
+    void setBusyHands(int ordinaryThief, boolean res) throws RemoteException;
 
     /**
      * Getter for the busy hands attribute of a thief.
-     * @param ordinaryThief the identification of the Ordinary Thief.
-     * @return whether or not they have a canvas.
+     * @param ordinaryThief the identification of the Ordinary Thief
+     * @return whether they have a canvas
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public boolean hasBusyHands(int ordinaryThief) throws RemoteException;
+    boolean hasBusyHands(int ordinaryThief) throws RemoteException;
 
     /**
      * Removes a member from the Assault Party.
-     * @param ordinaryThief the identification of the Ordinary Thief.
+     * @param ordinaryThief the identification of the Ordinary Thief
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void removeMember(int ordinaryThief) throws RemoteException;
+    void removeMember(int ordinaryThief) throws RemoteException;
 
     /**
      * Returns whether the Assault Party has no members.
-     * @return true if it is empty, false otherwise.
+     * @return true if it is empty, false otherwise
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public boolean isEmpty() throws RemoteException;
+    boolean isEmpty() throws RemoteException;
 
     /**
      * Sets the Assault Party in operation.
-     * @param inOperation true if the Assault Party is in operation, false otherwise.
+     * @param inOperation true if the Assault Party is in operation, false otherwise
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setInOperation(boolean inOperation) throws RemoteException;
+    void setInOperation(boolean inOperation) throws RemoteException;
 
     /**
      * Sets the members of the Assault Party.
-     * @param ordinaryThieves an array with the identification of the Ordinary Thieves.
+     * @param ordinaryThieves an array with the identifications of the Ordinary Thieves
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setMembers(int[] ordinaryThieves) throws RemoteException;
+    void setMembers(int[] ordinaryThieves) throws RemoteException;
 
     /**
      * Getter for if the Assault Party is in operation.
-     * @return true if it is in operation, false otherwise.
+     * @return true if it is in operation, false otherwise
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public boolean isInOperation() throws RemoteException;
+    boolean isInOperation() throws RemoteException;
 
     /**
      * Checks if a given thief is a member of the Assault Party.
-     * @param ordinaryThief the identification of the Ordinary Thief.
-     * @return true if affirmative, false otherwise.
+     * @param ordinaryThief the identification of the Ordinary Thief
+     * @return true if affirmative, false otherwise
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public boolean isMember(int ordinaryThief) throws RemoteException;
+    boolean isMember(int ordinaryThief) throws RemoteException;
 
     /**
      * Sets the room destination of the Assault Party.
-     * @param room the room identification.
-     * @param roomDistance the distance to the room.
-     * @param roomPaintings the number of paintings in the room.
+     * @param room the identification of the room
+     * @param roomDistance the distance to the room
+     * @param roomPaintings the number of paintings in the room
+     * @throws RemoteException if the execution of the remote code failed
      */
-    public void setRoom(int room, int roomDistance, int roomPaintings) throws RemoteException;
+    void setRoom(int room, int roomDistance, int roomPaintings) throws RemoteException;
 }
