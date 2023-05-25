0ee5ba67c10f7dd0edb79317ede0d024740e7a19
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 09:39:30 +0100

    deployAndRun scripts

--- deployAndRunFirstTime.sh
+++ deployAndRunFirstTime.sh
@@ -2,10 +2,17 @@ xterm  -T "RMI registry" -hold -e "./RMIRegistryDeployAndRun.sh" &
 sleep 4
 xterm  -T "Registry" -hold -e "./RegistryDeployAndRun.sh" &
 sleep 4
-xterm  -T "General Repository" -hold -e "./GeneralReposDeployAndRun.sh" &
+xterm  -T "General Repository" -hold -e "./GenReposDeployAndRun.sh" &
 sleep 2
-xterm  -T "Barber Shop" -hold -e "./BarberShopDeployAndRun.sh" &
+xterm  -T "Assault Party 0" -hold -e "./AP0DeployAndRun.sh" &
+sleep 2
+xterm  -T "Assault Party 1" -hold -e "./AP1DeployAndRun.sh" &
+sleep 2
+xterm  -T "Museum" -hold -e "./MuseumDeployAndRun.sh" &
+sleep 2
+xterm  -T "Collection Site" -hold -e "./CollSiteDeployAndRun.sh" &
+sleep 2
+xterm  -T "Concentration Site" -hold -e "./ConcSiteDeployAndRun.sh" &
 sleep 1
-xterm  -T "Barbers" -hold -e "./BarbersDeployAndRun.sh" &
-xterm  -T "Customers" -hold -e "./CustomersDeployAndRun.sh" &
+xterm  -T "Clients" -hold -e "./ClientDeployAndRun.sh" &
 # increase sleep times if needed
