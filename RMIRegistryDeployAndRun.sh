1d9b42ed7e0bda12e1b1e522db65e3c0c1d21684
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 10:43:38 +0100

    fixed port in RMIRegistryDeployAndRun

--- RMIRegistryDeployAndRun.sh
+++ RMIRegistryDeployAndRun.sh
@@ -8,4 +8,4 @@ echo "Decompressing data sent to the RMIregistry node."
 sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd test/HeistToTheMuseum ; unzip -uq dirRMIRegistry.zip'
 sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd test/HeistToTheMuseum/dirRMIRegistry ; cp interfaces/*.class /home/sd107/Public/classes/interfaces ; cp set_rmiregistry_d.sh /home/sd107'
 echo "Executing program at the RMIregistry node."
-sshpass -f password ssh sd107@l040101-ws01.ua.pt './set_rmiregistry_d.sh sd107 22161'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt './set_rmiregistry_d.sh sd107 22160'
