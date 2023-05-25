00df2dbe6c0e5efa53d4f2ed747f9e9b8ab71016
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 10:44:49 +0100

    buildAndGenerate now compiles to java 8

--- buildAndGenerateLocal.sh
+++ buildAndGenerateLocal.sh
@@ -1,5 +1,5 @@
 echo "Compiling source code."
-javac */*.java */*/*.java
+javac -source 1.8 -target 1.8 */*.java */*/*.java
 echo "Distributing intermediate code to the different execution environments."
 echo "  RMI registry"
 rm -rf dirRMIRegistry/interfaces
