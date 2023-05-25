b876e95ec80ea7db9d348ef6e9b9d248edb68e93
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 09:39:07 +0100

    RegistryDeployAndRun.sh

--- RegistryDeployAndRun.sh
+++ RegistryDeployAndRun.sh
@@ -1,7 +1,7 @@
 echo "Transfering data to the registry node."
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'mkdir -p test/SleepingBarbers'
-sshpass -f password scp dirRegistry.zip ruib@l040101-ws08.ua.pt:test/SleepingBarbers
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'mkdir -p test/HeistToTheMuseum'
+sshpass -f password scp dirRegistry.zip sd107@l040101-ws01.ua.pt:test/HeistToTheMuseum
 echo "Decompressing data sent to the registry node."
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'cd test/SleepingBarbers ; unzip -uq dirRegistry.zip'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd test/HeistToTheMuseum ; unzip -uq dirRegistry.zip'
 echo "Executing program at the registry node."
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'cd test/SleepingBarbers/dirRegistry ; ./registry_com_d.sh ruib'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd test/HeistToTheMuseum/dirRegistry ; ./registry_com_d.sh sd107'
