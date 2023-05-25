d712b9a51bb51a83dedea30c83d58919e58147c9
Diogo Santos <diogoejsantos@ua.pt>
2023-05-25 09:38:57 +0100

    RMIRegistryDeployAndRun.sh

--- RMIRegistryDeployAndRun.sh
+++ RMIRegistryDeployAndRun.sh
@@ -1,11 +1,11 @@
 echo "Transfering data to the RMIregistry node."
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'mkdir -p test/SleepingBarbers'
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'rm -rf test/SleepingBarbers/*'
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'mkdir -p Public/classes/interfaces'
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'rm -rf Public/classes/interfaces/*'
-sshpass -f password scp dirRMIRegistry.zip ruib@l040101-ws08.ua.pt:test/SleepingBarbers
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'mkdir -p test/HeistToTheMuseum'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'rm -rf test/HeistToTheMuseum/*'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'mkdir -p Public/classes/interfaces'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'rm -rf Public/classes/interfaces/*'
+sshpass -f password scp dirRMIRegistry.zip sd107@l040101-ws01.ua.pt:test/HeistToTheMuseum
 echo "Decompressing data sent to the RMIregistry node."
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'cd test/SleepingBarbers ; unzip -uq dirRMIRegistry.zip'
-sshpass -f password ssh ruib@l040101-ws08.ua.pt 'cd test/SleepingBarbers/dirRMIRegistry ; cp interfaces/*.class /home/ruib/Public/classes/interfaces ; cp set_rmiregistry_d.sh /home/ruib'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd test/HeistToTheMuseum ; unzip -uq dirRMIRegistry.zip'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd test/HeistToTheMuseum/dirRMIRegistry ; cp interfaces/*.class /home/sd107/Public/classes/interfaces ; cp set_rmiregistry_d.sh /home/sd107'
 echo "Executing program at the RMIregistry node."
-sshpass -f password ssh ruib@l040101-ws08.ua.pt './set_rmiregistry_d.sh ruib 22000'
+sshpass -f password ssh sd107@l040101-ws01.ua.pt './set_rmiregistry_d.sh sd107 22161'
