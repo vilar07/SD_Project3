059b35df080f6dbcd08c0df61755cb16559422ce
Diogo Santos <diogoejsantos@ua.pt>
2023-05-26 16:07:25 +0100

    updated docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"clientSide.entities"},{"l":"clientSide.main"},{"l":"clientSide.stubs"},{"l":"commInfra"},{"l":"serverSide.entities"},{"l":"serverSide.interfaces"},{"l":"serverSide.main"},{"l":"serverSide.sharedRegions"},{"l":"utils"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"clientSide.entities"},{"l":"clientSide.main"},{"l":"interfaces"},{"l":"serverSide.main"},{"l":"serverSide.objects"},{"l":"utils"}]
